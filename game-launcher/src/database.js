11f14fc009a2661a0221b783a6f2f55aa0990069
Amiru Weerathunga <amiru.upek.weerathunga@gmail.com>
2024-11-12 22:10:52 +0530

    First Realese Beta

--- game-launcher/src/database.js
+++ game-launcher/src/database.js
@@ -1,31 +1,24 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const { app } = require('electron');
 
-const dbPath = path.join(__dirname, '../userData/games.db');
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error('Error connecting to database:', err.message);
-  } else {
-    console.log('Connected to the SQLite database.');
-    db.run(`CREATE TABLE IF NOT EXISTS games (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      company TEXT,
-      path TEXT,
-      iconUrl TEXT,
-      coverUrl TEXT,
-      backgroundUrl TEXT,
-      lastOpened TEXT,
-      totalPlaytime INTEGER,
-      isRunning INTEGER
-    )`);
-  }
-});
+const dbPath = path.join(app.getPath('userData'), 'games.db');
+const db = new sqlite3.Database(dbPath);
 
-db.run(`ALTER TABLE games ADD COLUMN company TEXT`, (err) => {
-  if (err && !err.message.includes('duplicate column name')) {
-    console.error('Error adding company column:', err.message);
-  }
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS games (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    company TEXT,
+    path TEXT,
+    iconUrl TEXT,
+    textLogo TEXT,
+    coverUrl TEXT,
+    backgroundUrl TEXT,
+    lastOpened TEXT,
+    totalPlaytime INTEGER,
+    isRunning INTEGER
+  )`);
 });
 
 module.exports = db;
